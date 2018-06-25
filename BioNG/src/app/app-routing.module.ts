7cee6be7e84c2ccff0be30c816711e5d4ad4fd71
JH GDesk@SC <johan.haggqvist@gmail.com>
2018-06-25 15:49:33 +0200

    Bootstrap FA Setup

--- BioNG/src/app/app-routing.module.ts
+++ BioNG/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SalongComponent } from './salong/salong.component';
+import { FilmComponent } from './film/film.component';
+import { ShowComponent } from './show/show.component';
+
+const routes: Routes = [
+  {path: 'salong', component: SalongComponent },
+  {path: 'film', component: FilmComponent },
+  {path: 'show', component: ShowComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ SalongComponent, FilmComponent, ShowComponent ];
--- BioNG/src/app/app.module.ts
+++ BioNG/src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SalongComponent } from './salong/salong.component';
-import { FilmComponent } from './film/film.component';
-import { ShowComponent } from './show/show.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SalongComponent,
-    FilmComponent,
-    ShowComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
