e2afeae9ba75b8be0c0109a22233d532f6e821e1
JH GDesk@SC <johan.haggqvist@gmail.com>
2018-06-28 11:41:38 +0200

    New components seat/booking

--- BioNG/src/app/app.module.ts
+++ BioNG/src/app/app.module.ts
@@ -8,12 +8,16 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
+import { BookingComponent } from './salong/booking/booking.component';
+import { SeatComponent } from './salong/booking/seat/seat.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    BookingComponent,
+    SeatComponent
   ],
   imports: [
     BrowserModule,
